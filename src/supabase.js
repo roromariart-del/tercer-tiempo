26bdeb41de27c69b49ec1256eb9b598f7ddabf35
Mariangel <roromariart@gmail.com>
2026-06-11 00:30:58 -0600

    fix clave supabase anon

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cevwnhqpbmwftsncmazz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnduaHFwYm13ZnRzbmNtYXp6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDk2OTcyOSwiZXhwIjoyMDk2NTQ1NzI5fQ.65upATRHdnLHbTVYBkV81p_1R1mx_zc2XoQgQ6No5WU'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnduaHFwYm13ZnRzbmNtYXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5Njk3MjksImV4cCI6MjA5NjU0NTcyOX0.elw6d1CS8hnrCUGeW3lBriPpigWnWIFx-n9LSepl6O8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
